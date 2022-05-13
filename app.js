3c0689c85ac8c9b0f861c4e4af7efa783dbe6eeb
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-05-13 16:20:26 +0400

    to do list html css ve js-in bir hissesi bitdi

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
+const newNote = document.getElementById("note__box");
+const noteUl = document.querySelector(".list");
 const submiNote = document.getElementById("note");
+const elementLI = document.querySelector(".list");
+
 submiNote.addEventListener("submit", noteCreat);
+elementLI.addEventListener("click", doneAndDelete);
+
+function doneAndDelete(e) {
+  const clickELement = e.target;
+
+  if (clickELement.classList.contains("list__parent--check")) {
+    clickELement.parentElement.classList.toggle("list__done");
+    clickELement.parentElement.firstElementChild.classList.toggle(
+      "list__done--note"
+    );
+  } else if (clickELement.classList.contains("list__parent--delete")) {
+    clickELement.parentElement.classList.toggle("delete");
+    clickELement.parentElement.addEventListener("transitionend", (e) =>
+      clickELement.parentElement.remove()
+    );
+  }
+}
 
 function noteCreat(e) {
-  const noteValue = document.getElementById("note__box").value;
-  const noteDiv = document.createElement("div");
-  const noteLi = document.createElement("li");
-  const noteUl = document.querySelector(".list");
-  noteDiv.classList.add("list__parent");
-  noteLi.classList.add("list__note");
-  noteDiv.innerHTML = "<span class='icon first__icon'></span>";
-  noteUl.appendChild(noteDiv);
-  noteDiv.appendChild(noteLi);
-  noteLi.textContent = note.value;
-
-  noteLi.textContent = noteValue;
   e.preventDefault();
+  const noteValue = newNote.value;
+  // creat div
+  const noteDiv = document.createElement("div");
+  noteDiv.classList.add("list__parent");
+  // creat li
+  const noteLi = document.createElement("li");
+  noteLi.classList.add("list__note");
+  noteLi.textContent = noteValue;
+  noteDiv.appendChild(noteLi);
+
+  // creat check button
+  const buttonCheck = document.createElement("button");
+  buttonCheck.classList.add("list__parent--check");
+  buttonCheck.innerHTML = '<i class="far fa-check-square"></i>';
+  noteDiv.appendChild(buttonCheck);
+
+  // creat delete button
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("list__parent--delete");
+  buttonDelete.innerHTML = '<i class="far fa-trash-alt"></i>';
+  noteDiv.appendChild(buttonDelete);
+  newNote.value = "";
+  noteUl.appendChild(noteDiv);
 }
